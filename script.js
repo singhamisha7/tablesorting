d1085afcfbceaacf2ff916994e220c8623326ef5
singhamisha7 <singhamisha70700@gmail.com>
2023-11-23 13:43:52 +0530

    task added

---
+++ script.js
@@ -0,0 +1,10 @@
+const select = document.getElementById("select");
+
+select.addEventListener("change", function handleChange(event) {
+   console.log(event.target.value);
+});
+const choose = document.getElementById("choose");
+
+choose.addEventListener("change", function handleChange(event) {
+   console.log(event.target.value);
+});
